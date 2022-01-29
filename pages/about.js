ca3c54b70f137e86ff1364b9f7ca5440048bf12f
beomseok.choi <cbs412@naver.com>
2022-01-29 15:41:22 +0900

    pages, static pre rendering, routing, css modules

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import NavBar from "../components/navBar";
+
+export default function Tomato() {
+    return (
+    <div>
+        <NavBar />
+        <h1>About tomato!</h1>
+    </div>
+    )
+  
+}
